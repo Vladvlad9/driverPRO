dcf3508a51598c0bba805fb220e64acb61e59b8d
Vlad Paulechka <vlad9.ru@gmail.com>
2023-06-23 16:57:42 +0300

    update

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ async def on_startup(_):
 
         scheduler = AsyncIOScheduler()
 
-        scheduler.add_job(get_def_scheduler, trigger=CronTrigger(), hour='09', minute='00', kwargs={'bot': bot})
+        scheduler.add_job(get_def_scheduler, trigger=CronTrigger(), hour='9', minute='00', kwargs={'bot': bot})
         #scheduler.add_job(get_def_scheduler, trigger='cron', hour='10-21', minute='*/1', second='30', kwargs={'bot': bot})
 
         first_event_time = get_time[0][:-3]
